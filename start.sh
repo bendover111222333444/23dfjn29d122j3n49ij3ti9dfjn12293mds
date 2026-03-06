f37fe851198f1baf7b6bdfcb8f02be28ef231cf0
bendover111222333444 <sigmasigmaonthewallwhoisthe2@gmail.com>
2026-03-06 00:01:24 -0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 pip install wisp-python --break-system-packages
-python3 -m wisp.server --host 0.0.0.0 --port 5001 --proxy socks5://tlaqdnkf-rotate:7gehk0l6v9li@p.webshare.io:80 &
+python3 -m wisp.server --host 127.0.0.1 --port 5001 --proxy socks5://tlaqdnkf-rotate:7gehk0l6v9li@p.webshare.io:80 &
 sleep 2
 npm start
