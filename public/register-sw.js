972324c85613530c5bc44870a2dec5b5246da39e
bendover111222333444 <sigmasigmaonthewallwhoisthe2@gmail.com>
2026-03-08 01:32:31 -0800

    Update register-sw.js

--- public/register-sw.js
+++ public/register-sw.js
@@ -4,15 +4,21 @@ const stockSW = "./sw.js";
 const swAllowedHostnames = ["localhost", "127.0.0.1"];
 
 async function registerSW() {
-	if (!navigator.serviceWorker) {
-		if (
-			location.protocol !== "https:" &&
-			!swAllowedHostnames.includes(location.hostname)
-		)
-			throw new Error("Service workers cannot be registered without https.");
+    if (!navigator.serviceWorker) {
+        if (
+            location.protocol !== "https:" &&
+            !swAllowedHostnames.includes(location.hostname)
+        )
+            throw new Error("Service workers cannot be registered without https.");
+        throw new Error("Your browser doesn't support service workers.");
+    }
 
-		throw new Error("Your browser doesn't support service workers.");
-	}
+    const reg = await navigator.serviceWorker.register(stockSW);
 
-	await navigator.serviceWorker.register(stockSW);
+    if (reg.waiting && navigator.serviceWorker.controller) {
+        navigator.serviceWorker.controller.postMessage("abort");
+        await new Promise(resolve => {
+            navigator.serviceWorker.addEventListener("controllerchange", resolve, { once: true });
+        });
+    }
 }
