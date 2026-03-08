5c128edba08ecc286918969000832ff991be167d
bendover111222333444 <sigmasigmaonthewallwhoisthe2@gmail.com>
2026-03-08 01:50:10 -0800

    revert sw files

--- public/sw.js
+++ public/sw.js
@@ -8,23 +8,17 @@ self.addEventListener("activate", (event) => {
     event.waitUntil(clients.claim());
 });
 
-self.addEventListener("message", (event) => {
-    if (event.data === "abort") {
-        self.skipWaiting();
-    }
-});
-
 const { ScramjetServiceWorker } = $scramjetLoadWorker();
 const scramjet = new ScramjetServiceWorker();
 
 async function handleRequest(event) {
-    await scramjet.loadConfig();
-    if (scramjet.route(event)) {
-        return scramjet.fetch(event);
-    }
-    return fetch(event.request);
+	await scramjet.loadConfig();
+	if (scramjet.route(event)) {
+		return scramjet.fetch(event);
+	}
+	return fetch(event.request);
 }
 
 self.addEventListener("fetch", (event) => {
-    event.respondWith(handleRequest(event));
+	event.respondWith(handleRequest(event));
 });
